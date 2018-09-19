5fade813a1d4dc306018224a8c95eee34d54d7b4
Marx Roberto <marxmoita@gmail.com>
2018-09-19 17:17:38 -0300

    Evento add e evento onChange

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -5,10 +5,10 @@ import IconButton from '../template/iconButton'
 export default props => (
     <div role='form' className='todoForm'>
         <Grid cols='12 9 10'>
-            <input type='text' id='description' className='form-control' placeholder='Adicione uma tarefa'></input>
+            <input type='text' id='description' className='form-control' placeholder='Adicione uma tarefa' onChange={props.handleChange} value={props.description}></input>
         </Grid>
         <Grid cols='12 3 2'>
-            <IconButton style='primary' icon='plus'></IconButton>
+            <IconButton style='primary' icon='plus' onClick={props.handleAdd}></IconButton>
         </Grid>
     </div>
 )
