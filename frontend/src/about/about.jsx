8755ea45114ce5d1f73a7fe2dece6b996a22abfd
Marx Roberto <marxmoita@gmail.com>
2018-09-19 13:04:44 -0300

    Criando componente PageHeader

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name='Sobre' small="Nós" />
+
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus ducimus, repellat ullam nemo fugit nostrum dolorem sit, illum vitae, officiis temporibus. Vel fugiat odio aut est facilis esse sunt quaerat?</p>
+        <h2>Missão</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure minima vitae amet repellat consequuntur porro ducimus culpa aliquam necessitatibus soluta alias, ea nisi libero pariatur voluptatibus voluptates, ipsa unde facilis!</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sapiente natus magnam, asperiores ullam harum, omnis repellat ratione minus quaerat a est facilis provident earum voluptatum perspiciatis aliquam ipsam accusamus! Commodi.</p>
+
     </div>
 )
